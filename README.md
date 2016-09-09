380edb22b50fdf21b3d6eea829edca38cf660eb3
Charles Stein <charles.mst@gmail.com>
2016-09-09 16:05:42 -0300

    Init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ionic-phonegap-zip
 Generates phonegap build zip file from an ionic 2 Project
+
+ - Paste the code inside your gulpfile.js
+ - Install the dependencies
+ `npm install --save-dev gulp-replace gulp-zip merge-stream`
+ - Run the gulp task
+ `gulp phonegap-zip`
+ - The zip file has been generated inside the phonegap folder
+
+ PS: Dont forget to add the source="npm" attribute in all your plugins in config.xml
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+
+//Thanks to https://forum.ionicframework.com/t/building-ionic-2-beta-7-apps-with-phonegap-build/53253/2
+//Run the following commands 
+//$ npm install --save-dev gulp-replace 
+//$ npm install --save-dev gulp-zip
+//$ npm install --save-dev merge-stream
+//PS: Dont forget to add source="npm" tag in all plugins saved in config.xml
+var replace = require('gulp-replace');
+var zip = require('gulp-zip');
+var destFolder = 'platforms/phonegap';
+var merge = require('merge-stream');
+
+gulp.task('phonegap',['build'], function () {
+
+  var moveWww = gulp.src(['www/**/*.*'])
+    .pipe(gulp.dest(destFolder));
+  var moveResources = gulp.src(['resources/**/*.*'])
+    .pipe(gulp.dest(destFolder + '/resources'));
+
+  var moveConfig = gulp.src('config.xml')
+    .pipe(replace('xmlns:cdv="http://cordova.apache.org/ns/1.0"', 'xmlns:gap="http://phonegap.com/ns/1.0"'))
+    .pipe(replace(/(<\/?)plugin/g, '$1gap:plugin'))
+    .pipe(gulp.dest(destFolder));
+
+  return merge(moveWww, moveResources, moveConfig);
+});
+gulp.task('phonegap-zip', ['phonegap'], function () {
+  gulp.src(destFolder+'/**/*.*')
+    .pipe(zip('phonegap.zip'))
+    .pipe(gulp.dest('phonegap'));
+});
